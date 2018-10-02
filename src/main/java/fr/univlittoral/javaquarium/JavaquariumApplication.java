60139d496e91b95b855df47a841926e260cb65a8
simon <simon@debian.debian_simon>
2018-10-02 10:14:49 +0200

    init project JEE

---
+++ src/main/java/fr/univlittoral/javaquarium/JavaquariumApplication.java
@@ -0,0 +1,12 @@
+package fr.univlittoral.javaquarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaquariumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaquariumApplication.class, args);
+	}
+}
---
+++ src/test/java/fr/univlittoral/javaquarium/JavaquariumApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.univlittoral.javaquarium;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaquariumApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
